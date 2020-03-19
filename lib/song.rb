a2e252f186a6e4d8a232ec43b40819fcedb02426
Craig Christopher Messi <craig.messi@gmail.com>
2020-03-19 09:06:57 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -18,12 +18,12 @@ class Song
   end
   
   def self.new_by_filename(filename)
-    artist, song = filename.split(" - ")
-    new_song = self.new(song)      #creates new instance of a song from file that's passed
-    new_song.artist_name = artist       #associates new song instance with the artist from the filename
+    artist, song = filename.split(" - ")    #pars data(filename) to be readable and sets them equal to variables: artist, song
+    new_song = self.new(song)      #creates a "new instance of a song"(new_song) from file that's passed
+    new_song.artist_name = artist       #associates "new song instance"(new_song) with the artist from the filename
     new_song
   end 
   
- 
+  
     
 end
