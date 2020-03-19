43c61c6e133d934048e4b6a7c38826654fb30caf
Craig Christopher Messi <craig.messi@gmail.com>
2020-03-19 08:56:49 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -19,8 +19,8 @@ class Song
   
   def self.new_by_filename(filename)
     artist, song = filename.split(" - ")
-    new_song = self.new(song)
-    new_song.artist_name = artist
+    new_song = self.new(song)      #creates new instance of a song 
+    new_song.artist_name = artist       #sets artist object to belong to the song 
     new_song
   end 
     
