445617306f6fb441193b3d55b395caa1352fb9b0
Craig Christopher Messi <craig.messi@gmail.com>
2020-03-19 08:47:08 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -18,8 +18,9 @@ class Song
   end
   
   def self.new_by_filename(filename)
-    song = self.new 
-    song.name = filename
-    song
+    artist, song = filename.split(" - ")
+    new_song = self.new(song)
+    new_song.artist_name = artist
+    new_song
   end 
 end
