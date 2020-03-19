a3263176055da616fef5665adb1804ae9bd59038
Craig Christopher Messi <craig.messi@gmail.com>
2020-03-19 09:19:51 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -7,10 +7,6 @@ class Artist
     save
   end
   
-  def save
-    @@all << self 
-  end
-  
   def self.all 
     @@all 
   end
@@ -23,12 +19,13 @@ class Artist
     Song.all.select {|song| song.artist == self}
   end
 
-  def self.find_or_create_by_name(name)
-    self.find(name) ? self.find(name) : self.new(name)
-  end
 
   def self.find(name)
     self.all.find {|artist| artist.name == name }
+  end
+  
+  def save
+    @@all << self 
   end
 
   def print_songs
